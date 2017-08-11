0905ad3c2cbccddcd227154a5be3e26f553920c8
ronaldoexpert <contato@expertme.com.br>
2017-08-10 21:25:24 -0300

    Exercicio resolvido Calculo volume

--- src/aula/swing/pkg02/AulaSwing02.java
+++ src/aula/swing/pkg02/AulaSwing02.java
@@ -1,9 +1,23 @@
 package aula.swing.pkg02;
 
+import javax.swing.JOptionPane;
+
 public class AulaSwing02 {
 
     public static void main(String[] args) {
-        System.out.println("Testando");
+        String largura, altura, comprimento;
+        double volume = 0.0;
+        
+        largura = JOptionPane.showInputDialog("Digite a largura: ");
+        altura = JOptionPane.showInputDialog("Digite a altura: ");
+        comprimento = JOptionPane.showInputDialog("Digite a comprimento: ");
+        
+        volume = Double.parseDouble(largura) * Double.parseDouble(altura) * Double.parseDouble(comprimento);
+        
+        
+        JOptionPane.showMessageDialog(null, "O volume é : " + volume,
+                "Volume", JOptionPane.PLAIN_MESSAGE);
+    
     }
     
 }
